1094235dd9a981c53055dcac3c4312f408bbca6c
andvanri <andres.98V@hotmail.com>
2020-11-29 20:31:45 -0500

    Creacion de frontend en angular de Parnetships

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+import { ReactiveFormsModule } from '@angular/forms'; 
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDm0myjc_y0LYZWRmrMVXeoKL4PufyqJeo",
+    authDomain: "partnerships-52ae1.firebaseapp.com",
+    databaseURL: "https://partnerships-52ae1.firebaseio.com",
+    projectId: "partnerships-52ae1",
+    storageBucket: "partnerships-52ae1.appspot.com",
+    messagingSenderId: "870398076534",
+    appId: "1:870398076534:web:f0db867a9eb26232dfe157",
+    measurementId: "G-3JJMHSB6NZ"
+  }  
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
